5d359efa5326baf7ea05113a6751c6bd2323a891
David Beer <davebeer@hotmail.com>
2021-11-15 00:56:35 +1100

    trying yarn

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,7 +106,6 @@ module.exports = {
                     frontmatter {
                       title
                       date
-                      slug
                     }
                   }
                 }
