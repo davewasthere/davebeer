fd8477291bfa13c4020c1a533aa002cf45948e1b
Dave Beer <davebeer@hotmail.com>
2022-10-27 10:10:09 +1100

    tweak netlify config?

--- gatsby-config.js
+++ gatsby-config.js
@@ -132,7 +132,7 @@ module.exports = {
         icon: `src/images/site-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-react-helmet`,
+    // `gatsby-plugin-react-helmet`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
